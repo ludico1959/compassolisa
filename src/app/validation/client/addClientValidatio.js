bdfcbab0b5aff158a3a7c28c4e520a31204779c2
ludico1959 <ludico1959@gmail.com>
2021-10-31 22:08:37 -0300

    Setting client's name, CPF and email validation in addClientValidation file.

---
+++ src/app/validation/client/addClientValidatio.js
@@ -0,0 +1,36 @@
+const Joi = require('joi')
+
+module.exports = async (req, res, next) => {
+    try {
+        const schema = Joi.object({
+            nome: Joi.string()
+                .required(),
+
+            cpf: Joi.string()
+                .max(14)
+                .regex(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+                .required(),
+            
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .lowercase()
+                .required(),
+            
+            senha: Joi.string()
+                .min(6)
+                .required(),
+                
+            habilitado: Joi.string()
+                .valid('sim', 'não')
+                .required(),
+        })
+
+        const { error } = await schema.validate(req.body, { abortEarly: false })
+
+        if (error) throw error
+
+        return next()
+    } catch (error){
+        return res.status(400).json(error)
+    }
+}
