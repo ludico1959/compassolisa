a7d45910c3d8fc4cb451a4b35f2c6159ed16d088
ludico1959 <ludico1959@gmail.com>
2021-10-31 22:40:09 -0300

    Add update client validation.

---
+++ src/app/validation/client/updateClientValidation.js
@@ -0,0 +1,37 @@
+const Joi = require('joi')
+
+const now = Date.now()
+const cutoffDate = new Date(now - (1000 * 60 * 60 * 24 * 365 * 18))
+
+module.exports = async (req, res, next) => {
+    try {
+        const schema = Joi.object({
+            nome: Joi.string(),
+
+            cpf: Joi.string()
+                .max(14)
+                .regex(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/),
+            
+            data_nascimento: Joi.date()
+                .max(cutoffDate),
+
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .lowercase(),
+            
+            senha: Joi.string()
+                .min(6),
+                
+            habilitado: Joi.string()
+                .valid('sim', 'não'),
+        })
+
+        const { error } = await schema.validate(req.body, { abortEarly: false })
+
+        if (error) throw error
+
+        return next()
+    } catch (error){
+        return res.status(400).json(error)
+    }
+}
--- src/routes/client.router.js
+++ src/routes/client.router.js
@@ -1,13 +1,13 @@
 const ClientController = require('../app/controller/ClientController')
 const idValidation = require('../app/validation/client/idValidation')
-// const addClientValidation = require('../app/validation/client/addClientValidation')
-// const updateClientValidation = require('../app/validation/client/updateClientValidation')
+const addClientValidation = require('../app/validation/client/addClientValidation')
+const updateClientValidation = require('../app/validation/client/updateClientValidation')
 
 module.exports = (server, routes, prefix = '/api/v1/people') => {
-    routes.post('/', ClientController.addClient)
+    routes.post('/', addClientValidation, ClientController.addClient)
     routes.get('/', ClientController.listClients)
     routes.get('/:id', idValidation, ClientController.findClientById)
     routes.delete('/:id', idValidation, ClientController.removeClientById)
-    routes.put('/:id', idValidation, ClientController.updateClientById)
+    routes.put('/:id', idValidation, updateClientValidation, ClientController.updateClientById)
     server.use(prefix, routes)
 }
