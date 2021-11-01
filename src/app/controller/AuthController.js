c54302502aa9ca4883364134fc092295fcba8876
ludico1959 <ludico1959@gmail.com>
2021-11-01 01:52:46 -0300

    Add token to the authenticate controller through jwt module.

--- src/app/controller/AuthController.js
+++ src/app/controller/AuthController.js
@@ -1,21 +1,27 @@
 const ClientSchema = require('../schema/ClientSchema')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
 
 class AuthController {
     async authenticate(req, res) {
         const { email, senha } = await (req.body)
 
         const client = await ClientSchema.findOne({ email })
+        const habilitado = client.habilitado
 
         if (!client)
             return res.status(400).send({ error: 'Client not found.'})
-        
-        console.log(typeof senha, senha)
-        console.log(typeof client.senha, client.senha)
+
         if (senha != client.senha)
             return res.status(400).send({ error: 'Invalid password.'})
 
-            client.senha = undefined
-        return res.send({ client })
+        client.senha = undefined
+
+        const token = jwt.sign({ id: client.id }, authConfig.secret, {
+            expiresIn: 86400,
+        })
+
+        return res.status(201).json({ email, habilitado, token });
     }
 
 }
