be48cd9049f77df85427824b8a911e7fccae83f2
ludico1959 <ludico1959@gmail.com>
2021-11-01 17:29:19 -0300

    Install swagger-ui-express and create its middleware in app.js file

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = require('../routes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocs = require('./config/swagger.json')
 require('../infra/database/mongo')
 
 class AppController {
@@ -11,6 +13,7 @@ class AppController {
 
     middlewares() {
         this.server.use(express.json())
+        this.server.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
     }
 
     routes() {
