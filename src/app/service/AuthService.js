31fe5cfa00b142ada4e5cf801cb5429f2ce5aee7
ludico1959 <ludico1959@gmail.com>
2021-11-17 18:09:52 -0300

    fix (bug fix about encrypted password compare)

--- src/app/service/AuthService.js
+++ src/app/service/AuthService.js
@@ -6,13 +6,14 @@ const ClientRepository = require('../repository/ClientRepository');
 class AuthService {
   async authenticate(email, senha) {
     const client = await ClientRepository.findClientByEmail(email);
-    const { habilitado } = client;
 
     if (!client) throw Error('Client not found.');
 
-    bcrypt.compare(senha, client.senha, (error) => {
-      if (error) throw Error('Invalid password.');
-    });
+    const { habilitado } = client;
+
+    const checkPassword = await bcrypt.compare(senha, client.senha);
+
+    if (!checkPassword) throw Error('Invalid password.');
 
     client.senha = undefined;
 
